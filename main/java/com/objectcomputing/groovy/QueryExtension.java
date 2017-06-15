7a84a5443f719d4509bf6a68ddf5ed623ca67624
Matthew D Moss <mattmoss@splatbang.com>
2017-06-14 19:26:33 -0500

    Add Firebase Task-based Promise class. Create getValueAsync and getValue funcs for Query.

--- main/java/com/objectcomputing/groovy/QueryExtension.java
+++ main/java/com/objectcomputing/groovy/QueryExtension.java
@@ -1,11 +1,16 @@
 package com.objectcomputing.groovy;
 
 import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import groovy.lang.Closure;
 import groovy.lang.DelegatesTo;
+
+import java.util.concurrent.ExecutionException;
+
 
 public class QueryExtension {
 
@@ -163,6 +168,86 @@ public class QueryExtension {
         return self.addChildEventListener(
                 ChildEventListenerBuilder.create().onChildRemoved(closure).build()
         );
+    }
+
+    /**
+     * Get the value of this database query object. One-time read. Asynchronous.
+     *
+     * @param self Firebase database Query
+     * @return a Promise expected to receive the value of the snapshot at this location
+     */
+    public static Promise getValueAsync(Query self) {
+
+        Promise<Object> promise = new Promise<>();
+
+        self.addListenerForSingleValueEvent(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        promise.accept(dataSnapshot.getValue());
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        promise.accept(databaseError.toException());
+                    }
+                }
+        );
+
+        return promise;
+    }
+
+    /**
+     * Get the value of this database query object. One-time read. Asynchronous.
+     *
+     * @param self Firebase database Query
+     * @param valueType class into which snapshot value should be marshaled
+     * @return a Promise expected to receive the value of the snapshot at this location
+     */
+    public static <T> Promise<T> getValueAsync(Query self, Class<T> valueType) {
+
+        Promise<T> promise = new Promise<>();
+
+        self.addListenerForSingleValueEvent(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        promise.accept(dataSnapshot.getValue(valueType));
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        promise.accept(databaseError.toException());
+                    }
+                }
+        );
+
+        return promise;
+    }
+
+    /**
+     * Get the value of this database query object. One-time read. Synchronous.
+     *
+     * @param self Firebase database Query
+     * @return a Promise expected to receive the value of the snapshot at this location
+     * @throws InterruptedException if the current thread was interrupted while waiting
+     * @throws ExecutionException if the computation threw an exception
+     */
+    public static Object getValue(Query self) throws InterruptedException, ExecutionException {
+        return getValueAsync(self).get();
+    }
+
+    /**
+     * Get the value of this database query object. One-time read. Synchronous.
+     *
+     * @param self Firebase database Query
+     * @param valueType class into which snapshot value should be marshaled
+     * @return a Promise expected to receive the value of the snapshot at this location
+     * @throws InterruptedException if the current thread was interrupted while waiting
+     * @throws ExecutionException if the computation threw an exception
+     */
+    public static <T> T getValue(Query self, Class<T> valueType) throws InterruptedException, ExecutionException {
+        return getValueAsync(self, valueType).get();
     }
 
 }
