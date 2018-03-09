7a854be84a914f0aee4c209666e42d24f434997c
Matthew Moss <mattmoss@splatbang.com>
2018-03-09 14:42:34 -0600

    Build up more of main screen using react-bootstrap components

--- examples/chat-demo/client/src/App.js
+++ examples/chat-demo/client/src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { Grid, Row, Col } from 'react-bootstrap';
 import ChannelList from './ChannelList/ChannelList';
 
 class App extends React.Component {
     render() {
         return (
-            <div>
-                <ChannelList />
-            </div>
+            <Grid>
+                <Row>
+                    <Col sm={2}>
+                        <ChannelList />
+                    </Col>
+                    <Col sm={8}>
+                        <div></div>
+                    </Col>
+                    <Col sm={2}>
+                        <div></div>
+                    </Col>
+                </Row>
+            </Grid>
         );
     }
 }
