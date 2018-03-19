b2d5143a9ecb6a66b65619861bdf95a9a10ae644
Matthew Moss <mattmoss@splatbang.com>
2018-03-19 11:31:35 -0500

    Fix ChatService message processing to only remove incoming messaegs when outgoing has been written

--- examples/chat-demo/server/grails-app/services/chat/demo/ChatService.groovy
+++ examples/chat-demo/server/grails-app/services/chat/demo/ChatService.groovy
@@ -1,6 +1,7 @@
 package chat.demo
 
 import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 
@@ -38,11 +39,16 @@ class ChatService {
             // Get the incoming message, process it, and post it to outgoing (if it hasn't been vetoed).
             ChatMessage message = processMessage(channel, snapshot.getValue(ChatMessage))
             if (message) {
-                document["outgoing/${channel}"] << message
+                document["outgoing/${channel}"].push().setValue(message) { DatabaseError error, reference ->
+                    if (error) {
+                        log.error "Firebase Database error: ${error.message}"
+                    }
+                    else {
+                        // Once we've processed the message, remove it from incoming.
+                        incoming.remove snapshot.key
+                    }
+                }
             }
-
-            // Once we've processed the message, remove it from incoming.
-            incoming.remove snapshot.key
         }
 
         // Once we start listening to a channel, provide a way to stop listening.
